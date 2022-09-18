5da7e123b4da6b0d6fce77fa55501723bbefd5bd
Murilo Sousa Moraes <murilosousamoraes@gmail.com>
2022-09-17 21:48:04 -0300

    refactor: Criando a estrutura e adicionando configurações do banco e de startup

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Context
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Application.Context;
+
+namespace Application
+{
+    public class Startup
+    {
+        private readonly static string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public static void ConfigureServices(IServiceCollection service)
+        {
+            service.AddAuthentication(
+                CertificateAuthenticationDefaults.AuthenticationScheme)
+                .AddCertificate();
+
+            service.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins, policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+            service.AddControllers();
+
+            /*-------------------------------- DBCONTEXT --------------------------------*/
+            string connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=senha123;Database=localhost";
+            service.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
+        }
+
+        public static void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
